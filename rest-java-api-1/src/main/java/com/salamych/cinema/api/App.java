5cbe1f52a3081c20a6c0c65fc5cf9cd2cdd70f79
Ivan <volkov19@mail.ru>
2023-03-23 17:14:51 +0400

    Создание файлового хранилища

--- rest-java-api-1/src/main/java/com/salamych/cinema/api/App.java
+++ rest-java-api-1/src/main/java/com/salamych/cinema/api/App.java
@@ -4,39 +4,56 @@ import spark.Spark;
 import static spark.Spark.*;
 
 import com.salamych.cinema.api.controllers.ScheduleController;
+import com.salamych.cinema.api.models.ScheduleModel;
+import com.salamych.cinema.api.storages.FileStorage;
 
 import static com.salamych.cinema.api.transformers.JsonTransformer.JSON;
 
 public class App {
-    public final static String JSON_MIME_TYPE = "application/json"; 
-    
-    private final static String SERVER_HOST  = "127.0.0.1";
-    private final static int SERVER_PORT  = 8808;
-    
+
+    public final static String JSON_MIME_TYPE = "application/json";
+
+    private final static String SERVER_HOST = "127.0.0.1";
+    private final static int SERVER_PORT = 8808;
+
+    private final static String DEFAULT_FILE = "schedule.tsv";
 
     public static void main(String[] args) {
+
+        // создание файлового хранилища
+        final FileStorage<ScheduleModel> storage = new FileStorage<ScheduleModel>(
+                args != null && args.length == 1 ? args[0] : DEFAULT_FILE,
+                ScheduleModel.class
+        );
+
+        // чтение файла данных
+        storage.readAll();
+
+        // создание контроллера
         final ScheduleController controller = new ScheduleController();
-        
+
         // конфигурация сервера
         ipAddress(SERVER_HOST);
         port(SERVER_PORT);
-        
+
         // объявление роутов
-        path("/api", ()->{
-        get("/test",controller.test,JSON);
+        path("/api", () -> {
+            get("/test", controller.test, JSON);
         });
-        after((req, res)->{
-        res.type(JSON_MIME_TYPE);
+        after((req, res) -> {
+            res.type(JSON_MIME_TYPE);
         });
-        
+
         Runtime.getRuntime().addShutdownHook(new Thread() {
-        @Override
-        public void run(){
-        // остановка сервера
-        Spark.stop();
-        }
+            @Override
+            public void run() {
+                // остановка сервера
+                Spark.stop();
+                // сохранение данных в файл
+                storage.writeAll();
+            }
         });
-        
+
     }
-    
+
 }
---
+++ rest-java-api-1/src/main/java/com/salamych/cinema/api/storages/FileStorage.java
@@ -0,0 +1,66 @@
+package com.salamych.cinema.api.storages;
+
+import com.salamych.cinema.api.models.CommonModel;
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public final class FileStorage<T extends CommonModel> extends CommonStorage<T> {
+
+    // разделитель полей в строке
+    private final static String FIELD_SEPARATOR = "\t";
+
+    // имя файла для хранения данных
+    private final String fileName;
+
+    // конструктор
+    public FileStorage(final String fileName, final Class<T> dataClass) {
+        super(dataClass);
+
+        this.fileName = fileName;
+    }
+
+    // считать все данные из файла
+    public void readAll() {
+        this.dataList.clear();
+
+        final File file = new File(this.fileName);
+        if (file.exists() && file.isFile() && file.canRead()) {
+            try {
+                final RandomAccessFile raf = new RandomAccessFile(file, "r");
+                String utf;
+
+                while ((utf = raf.readUTF()) != null) {
+                    try {
+                        this.dataList.add(
+                                dataClass.getDeclaredConstructor(String[].class).newInstance(
+                                        new Object[]{utf.split(FIELD_SEPARATOR)}
+                                )
+                        );
+                    } catch (Exception exc) {
+                    }
+                }
+                raf.close();
+            } catch (IOException exc) {
+            }
+        }
+    }
+
+    // записать все данные в файл
+    public void writeAll() {
+        final File file = new File(this.fileName);
+        try {
+            final RandomAccessFile raf = new RandomAccessFile(file, "rw");
+
+            raf.setLength(0);
+            this.dataList.forEach((T dataItem) -> {
+                try {
+                    raf.writeUTF(String.join(FIELD_SEPARATOR, dataItem.toFields()));
+                } catch (IOException exc) {
+                }
+            });
+            raf.close();
+        } catch (IOException exc) {
+        }
+    }
+}
